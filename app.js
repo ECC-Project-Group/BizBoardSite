3a8381af87a7fff51215fb2e748ed5532f6deeb8
EL-MTN <masternode@163.com>
2021-10-31 19:18:40 -0400

    Created Sheets API, Updated Code Style

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", (_, res) => {
-	res.send("hello world");
+app.get('/', (_, res) => {
+  res.send('hello world');
 });
 
 app.listen(3030, () => {
-	console.log(`up and running on 3030`);
+  console.log('up and running on 3030');
 });
